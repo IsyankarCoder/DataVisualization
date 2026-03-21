b120a88dfa7c336efa6c04a0b9624aae04b2297f
Volkan Genç <gencvolkan@gmail.com>
2026-03-21 19:34:03 +0300

    scatter serpilme diagram

---
+++ scatter.py
@@ -0,0 +1,34 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+verisetim = pd.read_csv("insurance.csv")
+pd.set_option("display.max_columns",20)
+
+verisetim =verisetim.rename(columns={"age":"yas", "sex":"cinsiyet","bmi":"vki","children":"cocukSayisi","smoker":"sigaraDurum","region":"bolge","charges":"odemeMiktari"})
+verisetim["cinsiyet"]=verisetim["cinsiyet"].replace(["male","female"],["erkek","kadin"])
+verisetim["sigaraDurum"]=verisetim["sigaraDurum"].replace(["no","yes"],["evet","hayir"])
+verisetim["bolge"]=verisetim["bolge"].replace(["southeast","northwest","southwest","northeast"],
+                                              ["guneydogu","kuzeybati","guneybati","kuzeydogu"])
+
+print(verisetim.dtypes)
+verisetim["cinsiyet"]=verisetim["cinsiyet"].astype('category')
+verisetim["sigaraDurum"]= verisetim["sigaraDurum"].astype('category')
+verisetim["bolge"]=verisetim["bolge"].astype('category')
+verisetim.dtypes
+
+ 
+x=verisetim.yas
+y=verisetim.vki
+
+plt.scatter(x,y)
+
+plt.xlabel("Yaş")
+plt.ylabel("Vucüt Kitle İndeksi")
+ 
+plt.show()
+
+ 
+
+
---
+++ selectscatter.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+verisetim = pd.read_csv("insurance.csv")
+pd.set_option("display.max_columns",20)
+
+verisetim =verisetim.rename(columns={"age":"yas", "sex":"cinsiyet","bmi":"vki","children":"cocukSayisi","smoker":"sigaraDurum","region":"bolge","charges":"odemeMiktari"})
+verisetim["cinsiyet"]=verisetim["cinsiyet"].replace(["male","female"],["erkek","kadin"])
+verisetim["sigaraDurum"]=verisetim["sigaraDurum"].replace(["no","yes"],["evet","hayir"])
+verisetim["bolge"]=verisetim["bolge"].replace(["southeast","northwest","southwest","northeast"],
+                                              ["guneydogu","kuzeybati","guneybati","kuzeydogu"])
+
+print(verisetim.dtypes)
+verisetim["cinsiyet"]=verisetim["cinsiyet"].astype('category')
+verisetim["sigaraDurum"]= verisetim["sigaraDurum"].astype('category')
+verisetim["bolge"]=verisetim["bolge"].astype('category')
+verisetim.dtypes
+
+ 
+secim=[1072,548,1032,437,154,653,645,862,25,603]
+altkume = verisetim.iloc[secim,[0,6]]
+
+x=altkume.yas
+y=altkume.odemeMiktari
+
+plt.scatter(x,y)
+
+plt.xlabel("Yaş")
+plt.ylabel("Ödeme Miktarı")
+ 
+plt.show()
+
+ 
+
+
